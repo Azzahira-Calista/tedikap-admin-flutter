ca996c4a33bd22fbd3e9bbe01738b4ad32128ee7
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2024-04-30 00:00:57 +0700

    feat[CA]add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tedikap_admin/themes.dart';
-import 'package:tedikap_admin/views/start/splash_screen.dart';
+import 'package:tedikap_admin/views/home/home_admin.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
       ),
-      home: SplashAdmin(),
+      home: HomePage(),
     );
   }
 }
